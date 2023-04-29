d3bad1c562f4aa9df712ddcb465dd1ea1af8711b
Numan Kafadar <b21946242@cs.hacettepe.edu.tr>
2023-04-29 14:57:46 +0300

    code cleaning and adjustments

--- backend/src/main/java/com/cyberbullies/iceshu4/repository/UserRepository.java
+++ backend/src/main/java/com/cyberbullies/iceshu4/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.cyberbullies.iceshu4.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String username);
+    List<User> findAllByIdNot(Long id);
 }
