de33fe0e745583171cb48e1454c1149922e5eb1e
Yunus Emre Terzi <72995775+b2200356021@users.noreply.github.com>
2023-05-27 01:18:34 +0300

    Preauthorize

--- backend/src/main/java/com/cyberbullies/iceshu4/controller/CourseController.java
+++ backend/src/main/java/com/cyberbullies/iceshu4/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ public class CourseController {
         return new ResponseEntity<>("Course is created", HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/findAll")
     public List<Course> findAll() {
         return courseService.findAll();
